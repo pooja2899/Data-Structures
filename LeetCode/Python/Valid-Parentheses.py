69ee7e8361f46fe7dff09538f3afc254327e3312
Pooja Agarwal <poojaagarwal5757@gmail.com>
2022-05-21 14:16:34 +0530

    Update Valid-Parentheses.py

--- LeetCode/Python/Valid-Parentheses.py
+++ LeetCode/Python/Valid-Parentheses.py
@@ -1,1 +1,16 @@
-
+class Solution:
+    def isValid(self, s: str) -> bool:
+        bracket =  {'(':')','{':'}','[':']'}
+        open_bracket = set(['(','{','['])
+        stack=[]
+        
+        for i in s:
+            if i in open_bracket:
+                stack.append(i)
+                
+            elif stack and i == bracket[stack[-1]]:
+                stack.pop()
+                
+            else:
+                return False
+        return stack == []
